810a11116755737d165fb7615a086f0fcdcebaab
Caleb Costa <calebcosta@Calebs-MacBook-Pro.local>
2023-11-08 21:41:27 -0500

    Setting up repo.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+list = []
+
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
@@ -9,12 +11,10 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.event
 async def on_message(message):
-    if message.author == bot.user:
+    channel = bot.get_channel(message.channel.id)
+    past = [i.content async for i in channel.history(limit=None)] 
+    if message.author == bot.user or message.content != past[-1]:
         return 
 
     destination_channel_id = 1171944192390471765 
@@ -22,9 +22,11 @@ async def on_message(message):
 
     embed = discord.Embed(
         title=message.channel.name,
+        description=message.content,
         url=url,
         color=0x3498db 
     )
+
     embed.add_field(name="", value=message.author.mention, inline=False)
     for i in bot.get_channel(message.channel.id).applied_tags:
         embed.add_field(name='', value=f'`{i.name}`', inline=True)
@@ -32,4 +34,4 @@ async def on_message(message):
     destination_channel = bot.get_channel(destination_channel_id)
     await destination_channel.send(embed=embed)
 
-bot.run('MTE3MTk0NDYzMzUzODk3Nzg4Mg.GQhYSi.36pDEfk8RCso6ycgPnC4mMFxoMm84bRthCtE1c')
+bot.run('MTE3MTk0NDYzMzUzODk3Nzg4Mg.Gj1ZNQ.jyhdG64dUEBJ3YrUxytTx4gc4UQeQIbKPTH-UI')
